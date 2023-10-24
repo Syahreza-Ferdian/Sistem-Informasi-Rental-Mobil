b6b40aeaaa93da7c46a2a55b775c6d0a1e6ccb6e
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2023-10-25 00:24:57 +0700

    `update`: tampilan halaman utama jadi dark mode

--- nav.php
+++ nav.php
@@ -13,15 +13,21 @@
         endif
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$pageTitle?> | Toretto Rent Car</title>
+    <title><?=$pageTitle ?? 'Index'?> | Toretto Rent Car</title>
 
     <script src="https://kit.fontawesome.com/54ae9d808b.js" crossorigin="anonymous"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- <style>
+        .offcanvas-body .list-group .list-group-item span {
+            margin-left: 1rem;
+        }
+    </style> -->
 </head>
 <body>
     <div class="d-xxl-flex" style="overflow-x: hidden;">
@@ -32,14 +38,12 @@
                 </div>
                 <div class="offcanvas-body">
                     <div class="list-group list-group-flush">
-                        <a class="list-group-item list-group-item-action p-3" href="?page=index">Home</a>
-                        <a class="list-group-item list-group-item-action p-3" href="?page=daftar_mobil">Daftar Mobil</a>
-                        <a class="list-group-item list-group-item-action p-3" href="?page=transaksi">Transaksi</a>
-                        <a class="list-group-item list-group-item-action p-3" href="?page=pegawai">Pegawai</a>
-                        <a class="list-group-item list-group-item-action p-3" href="?page=customer">Customer Details</a>
-                        <a class="list-group-item list-group-item-action p-3" href="logout.php">Logout</a>
-                        <!-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a> -->
-                        <!-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a> -->
+                        <a class="list-group-item list-group-item-action p-3" href="?page=index"><div class="row"><div class="col-2"><i class="fa-solid fa-house fa-xl"></i></div><div class="col"><span>Home</span></div></div></a>
+                        <a class="list-group-item list-group-item-action p-3" href="?page=daftar_mobil"><div class="row"><div class="col-2"><i class="fa-solid fa-car-side fa-xl"></i></div><div class="col"><span>Daftar Mobil</span></div></div></a>
+                        <a class="list-group-item list-group-item-action p-3" href="?page=transaksi"><div class="row"><div class="col-2"><i class="fa-solid fa-sack-dollar fa-xl"></i></div><div class="col"><span>Transaksi</span></div></div></a>
+                        <a class="list-group-item list-group-item-action p-3" href="?page=pegawai"><div class="row"><div class="col-2"><i class="fa-solid fa-briefcase fa-xl"></i></div><div class="col"><span>Pegawai</span></div></div></a>
+                        <a class="list-group-item list-group-item-action p-3" href="?page=customer"><div class="row"><div class="col-2"><i class="fa-solid fa-users fa-xl"></i></div><div class="col"><span>Customer Details</span></div></div></a>
+                        <a class="list-group-item list-group-item-action p-3" href="logout.php"><div class="row"><div class="col-2"><i class="fa-solid fa-right-from-bracket fa-xl"></i></div><div class="col"><span>Logout</span></div></div></a>
                     </div>
                 </div>
                 <div class="offcanvas-footer">
@@ -51,7 +55,7 @@
             </div>
 
             <div class="page-content" style="min-width: 100vw;">
-                        <nav class="navbar" style="background-color: #e3f2fd;">
+                        <nav class="navbar bg-body-secondary border-bottom border-body" data-bs-theme="dark">
                             <!-- Navbar content -->
                             <div class="container-fluid">
                                 <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
