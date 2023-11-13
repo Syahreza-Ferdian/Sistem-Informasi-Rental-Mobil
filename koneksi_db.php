306c154a726bac5df5a1931ab6e783ea1baf1f11
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2023-11-14 00:02:38 +0700

    `file deletion`

--- koneksi_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $db_name = "rental_mobil";
-
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-    $koneksi = mysqli_connect($host, $user, $password, $db_name) or die("Terjadi error" .mysqli_connect_error());
